1ec11313c6412c79452308f777f7529950127260
Riche2000 <ricardosanch2000@gmail.com>
2025-11-01 20:23:20 -0600

    HAVING EXER

--- SQL_Course.sql
+++ SQL_Course.sql
@@ -104,3 +104,7 @@ SELECT store_id, COUNT(customer_id) FROM customer GROUP BY store_id HAVING COUNT
 SELECT customer_id, COUNT(payment_id) FROM payment GROUP BY customer_id HAVING COUNT(payment_id) > 39
 --CLIENTES QUE HAYAN COMPRADO AL MENOS $100 CON EL STAFF 2
 SELECT customer_id, staff_id, SUM(amount) FROM payment WHERE staff_id='2' GROUP BY customer_id, staff_id HAVING SUM(amount) > 100
+
+SELECT customer_id, staff_id, SUM(amount) FROM payment WHERE staff_id='2' GROUP BY customer_id, staff_id HAVING SUM(amount) > 110
+SELECT COUNT(*) FROM film WHERE title LIKE 'J%'
+SELECT customer_id, first_name, last_name, address_id FROM customer WHERE first_name LIKE 'E%' AND address_id<500 ORDER BY customer_id DESC LIMIT 1
